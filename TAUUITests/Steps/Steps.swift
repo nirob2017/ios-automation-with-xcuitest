01e078fffe0621b9ebfba64da267610d24cf359e
nirob2017 <nirob726@gmail.com>
2023-07-26 15:47:56 +0600

    Refactor tests, elements & steps

--- TAUUITests/Steps/Steps.swift
+++ TAUUITests/Steps/Steps.swift
@@ -1,50 +1,40 @@
 //
-//  BDDTest.swift
+//  Steps.swift
 //  TAUUITests
 //
 //  Created by Md. Monsur Hossain on 26/7/23.
-//  Copyright © Md. Monsur Hossain. All rights reserved.
+//  Copyright © 2023 Md. Monsur Hossain. All rights reserved.
 //
 
 import Foundation
 import XCTest
 
-class BDDTest: UITestBase {
-    
- func testThankYouMessageInBDStyle() {
-        givenAppIsReady()
-        whenIEnter(city: "London")
-        whenIEnrolled()
-        thenIShouldSeeThankYouMessage()
-    }
-}
-
 extension UITestBase {
     
     func givenAppIsReady() {
         XCTContext.runActivity(named: "Given App is ready ") { _ in
-            XCTAssertTrue(app.buttons["enrollButton"].exists)
+            XCTAssertTrue(Screen.enrollButton.element.exists)
         }
         
     }
     
     func whenIEnter(city: String) {
         XCTContext.runActivity(named: "When I enter city \(city) ") { _ in
-          app.textFields["city"].tap()
-          app.textFields["city"].typeText(city)
+            Screen.cityTextField.element.tap()
+            Screen.cityTextField.element.typeText(city)
         }
     }
     
     func whenIEnrolled() {
         XCTContext.runActivity(named: "When I Enrolled ") { _ in
-          app.buttons["enrollButton"].tap()
+            Screen.enrollButton.element.tap()
         }
         
     }
     
     func thenIShouldSeeThankYouMessage() {
         XCTContext.runActivity(named: "Then I Should See Thanks message ") { _ in
-         XCTAssertTrue(app.staticTexts["Thanks for Joining!"].exists)
+            XCTAssertTrue(Screen.thankYouMessage.element.exists)
         }
     }
     
