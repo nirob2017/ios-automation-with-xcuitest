c5b472a5b220b1e8f4114aa4f06f61a44acc7de8
mho-wtag <nirob726@gmail.com>
2023-07-26 10:55:16 +0600

    Revert "Add settings & test cases"
    This reverts commit f06472e7f3a79f2e91980e8b242b557f44b9e803.

--- TAUTests/TAUTests.swift
+++ TAUTests/TAUTests.swift
@@ -18,9 +18,9 @@ class TAUTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
